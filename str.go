1f88a47bb68ce48f67488d659d1b93bc183ffae9
kexplx <oscar.rosner@web.de>
2019-08-23 20:05:06 +0200

    takes current unix timestamp as seed for randn

--- str.go
+++ str.go
@@ -3,11 +3,16 @@ package randstr
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 var runes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 const n = 32
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // Get returns a pseudorandom string of length 32 from [a-zA-Z0-9]
 func Get(n int) (string, error) {
@@ -18,7 +23,7 @@ func Get(n int) (string, error) {
 	result := make([]rune, n)
 
 	for i := 0; i < n; i++ {
-		result[i] = runes[rand.Intn(len(runes))]
+		result[i] = runes[rand.Intn((len(runes)))]
 	}
 
 	return string(result), nil
